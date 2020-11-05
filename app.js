3b14c395273c3cf93833ee9357b1cedfbfd71214
Sivaprasad <sivaprasadev601@gmail.com>
2020-11-05 17:47:38 +0530

    app.js file modified for establishing mongodb

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var hbs = require('express-handlebars')
 var app = express();
 
 var fileUpload = require('express-fileupload')
-
+var db=require('./config/connection')
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -23,6 +23,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload())
+db.connect((err)=>{
+  if(err)
+  console.log("Connection Error"+err);
+  else
+  console.log("Databe Connected successfully")
+})
 app.use('/', userRouter);
 app.use('/admin', adminRouter);
 
